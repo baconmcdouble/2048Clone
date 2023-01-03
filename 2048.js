8de46fdd4b944c179bd5799b013d737e168e76d9
baconmcdouble <sihantang0316@gmail.com>
2023-01-02 23:10:34 -0500

    added slide left function

--- 2048.js
+++ 2048.js
@@ -1,7 +1,7 @@
 var board;
 var score = 0;
 var rows = 4;
-var colums = 4;
+var columns = 4;
 
 window.onload = function () {
     setGame();
@@ -10,13 +10,13 @@ window.onload = function () {
 function setGame() {
     board = [
         [0, 0, 0, 0],
-        [0, 0, 0, 0],
+        [2, 2, 2, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ]
 
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < colums; c++) {
+        for (let c = 0; c < columns; c++) {
             let tile = document.createElement('div');
             tile.id = r.toString() + '-' + c.toString();
             let num = board[r][c];
@@ -28,7 +28,7 @@ function setGame() {
 
 function updateTile(tile, num) {
     tile.innerText = "";
-    tile.classList.vale = "";
+    tile.classList.value = "";
     tile.classList.add("tile");
     if (num > 0) {
         tile.innerText = num;
@@ -36,6 +36,48 @@ function updateTile(tile, num) {
             tile.classList.add("x" + num.toString())
         } else {
             tile.classList.add('x8192')
+        }
+    }
+}
+
+document.addEventListener("keyup", (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+    }
+})
+
+function filterZero(row) {
+    return row.filter(num => num != 0);
+}
+
+function slide(row) {
+    row = filterZero(row);
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];
+        }
+    }
+    row = filterZero(row);
+
+    while (row.length < columns) {
+        row.push(0);
+    }
+    return row;
+}
+
+
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + '-' + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
         }
     }
 }
